5881a31a8d18109cfa4b925b842a7992d56d2fc0
Cmachado17 <machado.cristian1217@gmail.com>
2020-05-30 20:08:46 -0300

    se agrego react bootstrap, barra de navegacion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import BarraNavegacion from "./components/BarraNavegacion";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
   return (
     <div className="App">
-      <h1>Portfolio</h1>
+      <Row>
+        <Col sm={3}>
+          <BarraNavegacion />
+        </Col>
+        <Col sm={9}>
+          2
+        </Col>
+      </Row>
     </div>
   );
 }
---
+++ src/components/BarraNavegacion.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import Image from "react-bootstrap/Image";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const BarraNavegacion = () => {
+  return (
+    <Nav className="bg-success flex-column altura-nav">
+      <div className="my-5 p-2">
+        <Image
+          src="https://img.freepik.com/foto-gratis/pensativo-cliente-pensando-oferta-especial_74855-3588.jpg?size=626&ext=jpg"
+          rounded
+          className="img-fluid"
+        />
+      </div>
+        <Col className="float-to-top text-center">
+          <p className="h3">Cristian Machado</p>
+        </Col>
+        <Col className="float-to-bottom text-center">
+          <Nav.Link>SOBRE MI</Nav.Link>
+          <Nav.Link>PROYECTOS</Nav.Link>
+          <Nav.Link>EDUCACION</Nav.Link>
+          <Nav.Link>EXPERIENCIA LABORAL</Nav.Link>
+          <Nav.Link>CONTACTO</Nav.Link>
+        </Col>
+    
+    </Nav>
+  );
+};
+
+export default BarraNavegacion;
