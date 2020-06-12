f6646ad6ec483abafd0c661f3cae58010b5a0a9a
Cmachado17 <machado.cristian1217@gmail.com>
2020-06-12 17:24:35 -0300

    agregue separador de carousel y header, cambio de estilos

--- src/components/Carrusel.js
+++ src/components/Carrusel.js
@@ -1,34 +1,164 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import reactAdvisor from "./styles/img/reactadvisor.png";
+import tgifPage from "./styles/img/tgif-page.png";
+import NYSLPage from "./styles/img/NYSL-Page.png";
+import anotadorLlamadas from "./styles/img/anotador-llamadas.png";
+import gastoSemanal from "./styles/img/gasto-semanal.png";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Carrusel = () => {
   return (
-    <Carousel className="container">
-      <Carousel.Item>
-        <img
-          className="d-block w-100 altura-carousel"
-          src={reactAdvisor}
-          alt="First slide"
-        />
-        <div>
-          <div className="text-center">
-            <h3>React-Advisor</h3>
-            <p>Barrita de tecnologias usadas</p>
-          </div>
+    <Fragment>
+      <div className="mt-3 pb-1 text-center bg-dark text-white encabezado">
+        <h2>Proyectos</h2>
+      </div>
+      <Carousel className="container mt-3">
+        <Carousel.Item>
+          <img
+            className="d-block w-100 altura-carousel"
+            src={reactAdvisor}
+            alt="React-Advisor"
+          />
+          <div>
+            <Row className="text-center">
+              <Col sm={12} md={5}>
+                <h3>React-Advisor</h3>
+              </Col>
+              <Col sm={12} md={7}>
+                <p>Barrita de tecnologias usadas</p>
+              </Col>
+            </Row>
 
-          <p>
-            React-Advisor es el proyecto final que entregue en el curso de
-            Desarrollo de aplicaciones en React Js en Comunidad IT. Esta
-            pensando como una red social en la que se pueden registrar y hacer
-            reseñas sobre lugares de comida, bares, heladerias, etc. Ademas
-            cuenta con un apartado de administracion en el que se pueden
-            moderar/borrar los comentarios, dar de alta o de baja a nuevos
-            lugares, eliminar usuarios y una casilla de contacto.
-          </p>
-        </div>
-      </Carousel.Item>
-    </Carousel>
+            <p>
+              React-Advisor es el proyecto final que entregue en el curso de
+              Desarrollo de aplicaciones en React Js en Comunidad IT. Esta
+              pensando como una red social en la que se pueden registrar y hacer
+              reseñas sobre lugares de comida, bares, heladerias, etc. Ademas
+              cuenta con un apartado de administracion en el que se pueden
+              moderar/borrar los comentarios, dar de alta o de baja a nuevos
+              lugares, eliminar usuarios y una casilla de contacto.
+            </p>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 altura-carousel"
+            src={gastoSemanal}
+            alt="Gasto semanal"
+          />
+          <div>
+            <div className="text-center">
+              <h3>Gasto Semanal</h3>
+              <p>Barrita de tecnologias usadas</p>
+            </div>
+
+            <p>
+              React-Advisor es el proyecto final que entregue en el curso de
+              Desarrollo de aplicaciones en React Js en Comunidad IT. Esta
+              pensando como una red social en la que se pueden registrar y hacer
+              reseñas sobre lugares de comida, bares, heladerias, etc. Ademas
+              cuenta con un apartado de administracion en el que se pueden
+              moderar/borrar los comentarios, dar de alta o de baja a nuevos
+              lugares, eliminar usuarios y una casilla de contacto.
+            </p>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 altura-carousel"
+            src={anotadorLlamadas}
+            alt="Anotador Llamadas"
+          />
+          <div>
+            <div className="text-center">
+              <h3>Anotador Llamadas</h3>
+              <p>Barrita de tecnologias usadas</p>
+            </div>
+
+            <p>
+              React-Advisor es el proyecto final que entregue en el curso de
+              Desarrollo de aplicaciones en React Js en Comunidad IT. Esta
+              pensando como una red social en la que se pueden registrar y hacer
+              reseñas sobre lugares de comida, bares, heladerias, etc. Ademas
+              cuenta con un apartado de administracion en el que se pueden
+              moderar/borrar los comentarios, dar de alta o de baja a nuevos
+              lugares, eliminar usuarios y una casilla de contacto.
+            </p>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 altura-carousel"
+            src={gastoSemanal}
+            alt="Gasto semanal"
+          />
+          <div>
+            <div className="text-center">
+              <h3>Gasto Semanal</h3>
+              <p>Barrita de tecnologias usadas</p>
+            </div>
+
+            <p>
+              React-Advisor es el proyecto final que entregue en el curso de
+              Desarrollo de aplicaciones en React Js en Comunidad IT. Esta
+              pensando como una red social en la que se pueden registrar y hacer
+              reseñas sobre lugares de comida, bares, heladerias, etc. Ademas
+              cuenta con un apartado de administracion en el que se pueden
+              moderar/borrar los comentarios, dar de alta o de baja a nuevos
+              lugares, eliminar usuarios y una casilla de contacto.
+            </p>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 altura-carousel"
+            src={NYSLPage}
+            alt="NYSL Page"
+          />
+          <div>
+            <div className="text-center">
+              <h3>NYSL Page</h3>
+              <p>Barrita de tecnologias usadas</p>
+            </div>
+
+            <p>
+              React-Advisor es el proyecto final que entregue en el curso de
+              Desarrollo de aplicaciones en React Js en Comunidad IT. Esta
+              pensando como una red social en la que se pueden registrar y hacer
+              reseñas sobre lugares de comida, bares, heladerias, etc. Ademas
+              cuenta con un apartado de administracion en el que se pueden
+              moderar/borrar los comentarios, dar de alta o de baja a nuevos
+              lugares, eliminar usuarios y una casilla de contacto.
+            </p>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 altura-carousel"
+            src={tgifPage}
+            alt="TGIF government"
+          />
+          <div>
+            <div className="text-center">
+              <h3>TGIF government page</h3>
+              <p>Barrita de tecnologias usadas</p>
+            </div>
+
+            <p>
+              React-Advisor es el proyecto final que entregue en el curso de
+              Desarrollo de aplicaciones en React Js en Comunidad IT. Esta
+              pensando como una red social en la que se pueden registrar y hacer
+              reseñas sobre lugares de comida, bares, heladerias, etc. Ademas
+              cuenta con un apartado de administracion en el que se pueden
+              moderar/borrar los comentarios, dar de alta o de baja a nuevos
+              lugares, eliminar usuarios y una casilla de contacto.
+            </p>
+          </div>
+        </Carousel.Item>
+      </Carousel>
+    </Fragment>
   );
 };
 
